bb9b6dffe8135167cdcbaf5fe78906a314e70a1d
Morgan Selbekk <mselbekk11@gmail.com>
2023-07-25 16:15:13 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ import Thoughts from './components/Thoughts'
 import Videos from './components/Videos'
 import Newsletter from './components/Newsletter'
 import Footer from './components/Footer'
-import Fade from 'react-reveal/Fade'
 
 function App() {
   return (
     <div className='App'>
-      <Fade top distance='0%' duration={1500}>
-        <Navigation />
-        <Hero />
-        <About />
-        <Thoughts />
-        <Videos />
-        <Newsletter />
-        <Footer />
-      </Fade>
+      <Navigation />
+      <Hero />
+      <About />
+      <Thoughts />
+      <Videos />
+      <Newsletter />
+      <Footer />
     </div>
   )
 }
