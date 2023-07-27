1b034de129edbf390b8e5d1f85ce0d4d71bcf6f7
Morgan Selbekk <mselbekk11@gmail.com>
2023-07-27 12:17:02 +0100

    boom

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,17 +1,17 @@
-function Videos() {
-  return (
-    <div className="videos">
-      <h2>Videos</h2>
-      <div className="container">
-        <div className="video-left">
-        <iframe width="100%" height="315" src="https://www.youtube.com/embed/Y4_E0HOeCY0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-        <div className="video-right">
-        <iframe width="100%" height="315" src="https://www.youtube.com/embed/v0vkmJxGs2M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-      </div>
-    </div>
-  )
-}
+// function Videos() {
+//   return (
+//     <div className="videos">
+//       <h2>Videos</h2>
+//       <div className="container">
+//         <div className="video-left">
+//         <iframe width="100%" height="315" src="https://www.youtube.com/embed/Y4_E0HOeCY0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+//         </div>
+//         <div className="video-right">
+//         <iframe width="100%" height="315" src="https://www.youtube.com/embed/v0vkmJxGs2M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Videos
+// export default Videos
