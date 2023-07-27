d83aa5b1db23ed2125cf8aa0d83579709996d829
Morgan Selbekk <mselbekk11@gmail.com>
2023-07-27 12:19:47 +0100

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="about">
       <h2>About</h2>
-      <p>I believe in being deliberate with my actions, intentional with my words, and acting and living with purpose, both personally and professionally.</p>
+      <p>I believe in being deliberate with my actions, intentional with my words, and acting and living with purpose, both personally and professionally..</p>
 
 <p>To achieve these goals, I force myself to step out of my comfort zone and to think outside of the box.</p>
 
